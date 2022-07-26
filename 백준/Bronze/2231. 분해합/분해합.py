e68823e632758c2264f86eb285977ffb912195eb
runedemonic <runedemonic@naver.com>
2022-07-26 18:26:25 +0900

    [Bronze II] Title: 분해합, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -1,8 +1,12 @@
 a=input()
-b= map(int,a)
-#scan = 9*len(list(b))
-#int(a)-scan
-for i in range(1,int(a)+1):
+
+b= list(map(int,a))
+
+scan = 9*len(b)
+
+start = int(a)-scan
+
+for i in range(max(1,start),int(a)+1):
     result = i + sum(map(int,str(i)))
     if result == int(a):
         print(i)
