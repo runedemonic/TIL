fb8e51f73cef232e1d5bad181e381c71112e99e5
runedemonic <runedemonic@naver.com>
2022-08-09 15:01:22 +0900

    [Silver III] Title: 바이러스, Time: 92 ms, Memory: 32428 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,12 +1,27 @@
 from collections import defaultdict
 
-def dfs(graph, start, visited=[]):
-    ## 데이터를 추가하는 명령어 / 재귀가 이루어짐
-    visited.append(start)
 
-    for node in graph[start]:
+def dfs(graph, start_node):
+    ## deque 패키지 불러오기
+    from collections import deque
+    visited = []
+    need_visited = deque()
+
+    ##시작 노드 설정해주기
+    need_visited.append(start_node)
+
+    ## 방문이 필요한 리스트가 아직 존재한다면
+    while need_visited:
+        ## 시작 노드를 지정하고
+        node = need_visited.pop()
+
+        ##만약 방문한 리스트에 없다면
         if node not in visited:
-            dfs(graph, node, visited)
+            ## 방문 리스트에 노드를 추가
+            visited.append(node)
+            ## 인접 노드들을 방문 예정 리스트에 추가
+            need_visited.extend(graph[node])
+
     return visited
 
 All_c = int(input())
